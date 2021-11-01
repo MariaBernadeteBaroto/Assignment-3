f0c562ce50f1e9d1af6678dceaf0996271bcfcda
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-10-31 23:56:51 -0700

    first commit

---
+++ file1.py
@@ -0,0 +1,7 @@
+def personal_info():
+    name = input('Enter your name: ')
+    age = input('Enter your age: ')
+    address = input('Enter your address: ')
+    print(f'Hello, my name is {name}. I am {age} years old and I live in {address}.')
+
+personal_info()
