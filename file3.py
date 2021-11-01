f6ad4c7abbd0d29784e35c5fe7d65f48a35c696c
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-01 01:32:56 -0700

    commit

---
+++ file3.py
@@ -0,0 +1,7 @@
+import math
+amount_of_money = int(input('Enter Amount of Money: '))
+apple = int(input('How much apple?: '))
+price_apple = apple*15
+change = amount_of_money - price_apple
+number_of_apple = math.floor(amount_of_money/15)
+print(f'You can buy {number_of_apple} apples and your change is {change}.')
