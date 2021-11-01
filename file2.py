68a5d95e927054f7d65c0a7c349b9567cbfc4593
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-11-01 00:55:56 -0700

    icommit

---
+++ file2.py
@@ -0,0 +1,7 @@
+number_of_apple = int(input('how many apples?: '))
+apple = number_of_apple * 20
+number_of_orange = int(input('how many orange?: '))
+orange = number_of_orange * 25
+total = orange + apple
+print('The Total Amount is')
+print(total)
